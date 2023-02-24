455916ee4d82fbaf5cabd01db4ca684ed9598067
Tomohiko OKAZAKI <t-okazaki@groovenauts.jp>
2023-02-24 17:18:47 +0900

    support to read data from google cloud storage.

--- cli/src/df.rs
+++ cli/src/df.rs
@@ -5,8 +5,12 @@ use datafusion::prelude::{
     CsvReadOptions, DataFrame, NdJsonReadOptions, ParquetReadOptions, SessionConfig, SessionContext,
 };
 use std::ffi::OsStr;
+use std::fs::remove_dir_all;
 use std::path::Path;
 use thiserror::Error;
+use object_store::gcp::GoogleCloudStorageBuilder;
+use std::sync::Arc;
+use url::Url;
 
 #[derive(Debug, Args)]
 pub struct DataFusionArgs {
@@ -25,6 +29,10 @@ pub struct DataFusionArgs {
     /// The result is always shown in stdout. This option write the result to the file.
     #[clap(short = 'o', long = "output", default_value = None)]
     pub output: Option<String>,
+
+    /// If Output argument file exists, force to remove.
+    #[clap(short = 'r', long = "remove", default_value = "false")]
+    pub remove: bool
 }
 
 #[derive(Debug, Subcommand)]
@@ -45,8 +53,12 @@ pub enum DFError {
     UnsupportFileFormat,
 }
 
-pub async fn write_file(df: DataFrame, filename: String) -> Result<()> {
-    if let Some(output_ex) = Path::new(&filename).extension().and_then(OsStr::to_str) {
+pub async fn write_file(df: DataFrame, filename: String, remove: bool) -> Result<()> {
+    let path = Path::new(&filename);
+    if let Some(output_ex) = path.extension().and_then(OsStr::to_str) {
+        if path.exists() && remove {
+            remove_dir_all(&filename)?;
+        }
         match output_ex {
             "json" => {
                 df.write_json(&filename).await?;
@@ -70,9 +82,28 @@ pub async fn write_file(df: DataFrame, filename: String) -> Result<()> {
 pub async fn handle(dfargs: DataFusionArgs) -> Result<()> {
     let cfg = SessionConfig::new().with_information_schema(true);
     let ctx = SessionContext::with_config(cfg);
+
     for (i, input) in dfargs.inputs.iter().enumerate() {
         let table_id = format!("t{}", i);
-        if let Some(input_ex) = Path::new(input).extension().and_then(OsStr::to_str) {
+
+        if let Ok(url) = Url::parse(input) {
+            match url.scheme() {
+                "gs" => {
+                    if let Some(bucket_name) = url.host_str() {
+                        let gcs = GoogleCloudStorageBuilder::new()
+                            .with_service_account_path(std::env::var("GOOGLE_APPLICATION_CREDENTIALS")?)
+                            .with_bucket_name(bucket_name)
+                            .build()?;
+                    ctx.runtime_env()
+                        .register_object_store(url.scheme(), bucket_name, Arc::new(gcs));
+                    }
+                },
+                _ => {}
+            }
+        }
+
+        let path = Path::new(input);
+        if let Some(input_ex) = path.extension().and_then(OsStr::to_str) {
             match input_ex {
                 "json" => {
                     ctx.register_json(&table_id, input, NdJsonReadOptions::default())
@@ -97,7 +128,7 @@ pub async fn handle(dfargs: DataFusionArgs) -> Result<()> {
             let df = ctx.sql(&args.query).await?;
             df.clone().show().await?;
             if let Some(output) = dfargs.output {
-                write_file(df, output).await?;
+                write_file(df, output, dfargs.remove).await?;
             }
             Ok(())
         }
