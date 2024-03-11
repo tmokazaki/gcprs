2ab5f21f2f1e51889252c03059545daa04c0f95d
Tomohiko OKAZAKI <t-okazaki@groovenauts.jp>
2024-03-11 20:58:13 +0900

    wip

--- src/run.rs
+++ src/run.rs
@@ -1,18 +1,22 @@
-pub mod service;
+pub mod execution;
 pub mod job;
+pub mod service;
 use crate::auth;
 
 use anyhow;
 use anyhow::Result;
-use google_run2 as cloud_run;
 use cloud_run::{
     api::{
         GoogleCloudRunV2CloudSqlInstance, GoogleCloudRunV2Container, GoogleCloudRunV2EnvVar,
-        GoogleCloudRunV2RevisionScaling,
-        GoogleCloudRunV2Volume, GoogleCloudRunV2VolumeMount,
+        GoogleCloudRunV2Execution, GoogleCloudRunV2Job, GoogleCloudRunV2ListExecutionsResponse,
+        GoogleCloudRunV2ListJobsResponse, GoogleCloudRunV2ListServicesResponse,
+        GoogleCloudRunV2ResourceRequirements, GoogleCloudRunV2Service, GoogleCloudRunV2Volume,
+        GoogleCloudRunV2VolumeMount, GoogleLongrunningOperation, GoogleCloudRunV2RunJobRequest,
     },
     CloudRun as GcpCloudRun, Error, Result as GcpResult,
 };
+use google_run2 as cloud_run;
+use hyper::{Body, Response};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
@@ -23,6 +27,12 @@ pub struct VolumeMount {
 }
 
 impl VolumeMount {
+    fn to_volume_mount(&self) -> GoogleCloudRunV2VolumeMount {
+        let mut volume_mount = GoogleCloudRunV2VolumeMount::default();
+        volume_mount.name = Some(self.name.clone());
+        volume_mount.mount_path = Some(self.mount_path.clone());
+        volume_mount
+    }
     pub fn from_volume_mount(vm: &GoogleCloudRunV2VolumeMount) -> Self {
         let name = vm.name.as_ref().map(|n| n.clone()).unwrap();
         let mount_path = vm.mount_path.as_ref().map(|p| p.clone()).unwrap();
@@ -73,6 +83,31 @@ pub struct Container {
 impl Container {
     fn to_container(&self) -> GoogleCloudRunV2Container {
         let mut container = GoogleCloudRunV2Container::default();
+        container.image = Some(self.image.clone());
+        container.args = Some(self.args.clone());
+        container.command = Some(self.command.clone());
+        if 0 < self.volume_mounts.len() {
+            container.volume_mounts = Some(
+                self.volume_mounts
+                    .iter()
+                    .map(|v| v.to_volume_mount())
+                    .collect(),
+            )
+        }
+        container.env = Some(
+            self.env
+                .iter()
+                .map(|(k, v)| GoogleCloudRunV2EnvVar {
+                    name: Some(k.clone()),
+                    value: Some(v.clone()),
+                    value_source: None,
+                })
+                .collect(),
+        );
+        let mut resources = GoogleCloudRunV2ResourceRequirements::default();
+        resources.limits = Some(self.resources.clone());
+        resources.startup_cpu_boost = Some(true);
+        container.resources = Some(resources);
         container
     }
     fn from_container(container: &GoogleCloudRunV2Container) -> Self {
@@ -146,7 +181,7 @@ impl CloudRun {
         Ok(CloudRun { api: hub })
     }
 
-    fn handle_error<T>(result: GcpResult<T>) -> Result<T> {
+    fn handle_error<T, E>(result: GcpResult<T>, f: &dyn Fn(T) -> Result<E>) -> Result<E> {
         match result {
             Err(e) => match e {
                 // The Error enum provides details about what exactly happened.
@@ -165,11 +200,20 @@ impl CloudRun {
                     Err(anyhow::anyhow!("{}", e))
                 }
             },
-            Ok(res) => Ok(res),
+            Ok(res) => f(res),
         }
     }
 
-    pub async fn services_get(&self, service_name: &service::RunServiceName) -> Result<service::Service> {
+    fn response_to_service(
+        resp: (Response<Body>, GoogleCloudRunV2Service),
+    ) -> Result<service::Service> {
+        service::Service::from_service(&resp.1)
+    }
+
+    pub async fn services_get(
+        &self,
+        service_name: &service::RunServiceName,
+    ) -> Result<service::Service> {
         let resp = self
             .api
             .projects()
@@ -177,30 +221,29 @@ impl CloudRun {
             .doit()
             .await;
         println!("{:?}", resp);
-        match resp {
-            Err(e) => match e {
-                Error::BadRequest(_) => {
-                    eprintln!("{}", e);
-                    Err(anyhow::anyhow!("{}", e))
-                }
-                Error::HttpError(_)
-                | Error::Io(_)
-                | Error::MissingAPIKey
-                | Error::MissingToken(_)
-                | Error::Cancelled
-                | Error::UploadSizeLimitExceeded(_, _)
-                | Error::Failure(_)
-                | Error::FieldClash(_)
-                | Error::JsonDecodeError(_, _) => {
-                    eprintln!("{}", e);
-                    Err(anyhow::anyhow!("{}", e))
-                }
-            },
-            Ok(resp) => service::Service::from_service(&resp.1),
-        }
+        CloudRun::handle_error(resp, &CloudRun::response_to_service)
     }
 
-    pub async fn services_list(&self, service_name: &service::RunServiceName) -> Result<Vec<service::Service>> {
+    fn response_to_list_services(
+        resp: (Response<Body>, GoogleCloudRunV2ListServicesResponse),
+    ) -> Result<Vec<service::Service>> {
+        Ok(resp
+            .1
+            .services
+            .as_ref()
+            .map(|services| {
+                services
+                    .iter()
+                    .map(|service| service::Service::from_service(service).unwrap())
+                    .collect()
+            })
+            .unwrap_or_else(|| vec![]))
+    }
+
+    pub async fn services_list(
+        &self,
+        service_name: &service::RunServiceName,
+    ) -> Result<Vec<service::Service>> {
         let resp = self
             .api
             .projects()
@@ -208,68 +251,85 @@ impl CloudRun {
             .doit()
             .await;
         println!("{:?}", resp);
-        match resp {
-            Err(e) => match e {
-                Error::BadRequest(_) => {
-                    eprintln!("{}", e);
-                    Err(anyhow::anyhow!("{}", e))
-                }
-                Error::HttpError(_)
-                | Error::Io(_)
-                | Error::MissingAPIKey
-                | Error::MissingToken(_)
-                | Error::Cancelled
-                | Error::UploadSizeLimitExceeded(_, _)
-                | Error::Failure(_)
-                | Error::FieldClash(_)
-                | Error::JsonDecodeError(_, _) => {
-                    eprintln!("{}", e);
-                    Err(anyhow::anyhow!("{}", e))
-                }
-            },
-            Ok(resp) => Ok(resp
-                .1
-                .services
-                .as_ref()
-                .map(|services| services.clone())
-                .unwrap_or_else(|| vec![])
-                .iter()
-                .map(|service| service::Service::from_service(&service).unwrap())
-                .collect()),
-        }
+        CloudRun::handle_error(resp, &CloudRun::response_to_list_services)
     }
-    pub async fn jobs_get(&self, service_name: &job::RunJobName) -> Result<()> {
+
+    fn response_to_job(resp: (Response<Body>, GoogleCloudRunV2Job)) -> Result<job::Job> {
+        job::Job::from_job(&resp.1)
+    }
+
+    pub async fn jobs_get(&self, job_name: &job::RunJobName) -> Result<job::Job> {
         let resp = self
             .api
             .projects()
-            .locations_jobs_get(&service_name.name())
+            .locations_jobs_get(&job_name.name())
             .doit()
             .await;
         println!("{:?}", resp);
-        match resp {
-            Err(e) => match e {
-                Error::BadRequest(_) => {
-                    eprintln!("{}", e);
-                    Err(anyhow::anyhow!("{}", e))
-                }
-                Error::HttpError(_)
-                | Error::Io(_)
-                | Error::MissingAPIKey
-                | Error::MissingToken(_)
-                | Error::Cancelled
-                | Error::UploadSizeLimitExceeded(_, _)
-                | Error::Failure(_)
-                | Error::FieldClash(_)
-                | Error::JsonDecodeError(_, _) => {
-                    eprintln!("{}", e);
-                    Err(anyhow::anyhow!("{}", e))
-                }
-            },
-            Ok(resp) => Ok(()),
-        }
+        CloudRun::handle_error(resp, &CloudRun::response_to_job)
     }
 
-    pub async fn jobs_list(&self, job_name: &job::RunJobName) -> Result<Vec<()>> {
+    fn response_to_operation(resp: (Response<Body>, GoogleLongrunningOperation)) -> Result<()> {
+        println!("{:?}", &resp.1);
+        Ok(())
+    }
+
+    fn response_operation_to_job(resp: (Response<Body>, GoogleLongrunningOperation)) -> Result<job::Job> {
+        let job_json = serde_json::to_string(&resp.1.metadata.unwrap()).unwrap();
+        let j: GoogleCloudRunV2Job = serde_json::from_str(&job_json).unwrap();
+        job::Job::from_job(&j)
+    }
+
+    pub async fn jobs_create(&self, job_name: &job::RunJobName, job: &job::Job) -> Result<job::Job> {
+        let resp = self
+            .api
+            .projects()
+            .locations_jobs_create(job.to_job(), &job_name.parent())
+            .job_id(job_name.job_name().unwrap())
+            .doit()
+            .await;
+        // println!("{:?}", resp);
+        CloudRun::handle_error(resp, &CloudRun::response_operation_to_job)
+    }
+    pub async fn jobs_delete(&self, job_name: &job::RunJobName) -> Result<job::Job> {
+        let resp = self
+            .api
+            .projects()
+            .locations_jobs_delete(&job_name.name())
+            .doit()
+            .await;
+        // println!("{:?}", resp);
+        CloudRun::handle_error(resp, &CloudRun::response_operation_to_job)
+    }
+    pub async fn jobs_run(&self, job_name: &job::RunJobName) -> Result<job::Job> {
+        // TODO: accept override parameters?
+        let req = GoogleCloudRunV2RunJobRequest::default();
+        let resp = self
+            .api
+            .projects()
+            .locations_jobs_run(req, &job_name.name())
+            .doit()
+            .await;
+        // println!("{:?}", resp);
+        CloudRun::handle_error(resp, &CloudRun::response_operation_to_job)
+    }
+
+    fn response_to_list_jobs(
+        resp: (Response<Body>, GoogleCloudRunV2ListJobsResponse),
+    ) -> Result<Vec<job::Job>> {
+        Ok(resp
+            .1
+            .jobs
+            .as_ref()
+            .map(|jobs| {
+                jobs.iter()
+                    .map(|job| job::Job::from_job(job).unwrap())
+                    .collect()
+            })
+            .unwrap_or_else(|| vec![]))
+    }
+
+    pub async fn jobs_list(&self, job_name: &job::RunJobName) -> Result<Vec<job::Job>> {
         let resp = self
             .api
             .projects()
@@ -277,26 +337,68 @@ impl CloudRun {
             .doit()
             .await;
         println!("{:?}", resp);
-        match resp {
-            Err(e) => match e {
-                Error::BadRequest(_) => {
-                    eprintln!("{}", e);
-                    Err(anyhow::anyhow!("{}", e))
-                }
-                Error::HttpError(_)
-                | Error::Io(_)
-                | Error::MissingAPIKey
-                | Error::MissingToken(_)
-                | Error::Cancelled
-                | Error::UploadSizeLimitExceeded(_, _)
-                | Error::Failure(_)
-                | Error::FieldClash(_)
-                | Error::JsonDecodeError(_, _) => {
-                    eprintln!("{}", e);
-                    Err(anyhow::anyhow!("{}", e))
-                }
-            },
-            Ok(resp) => Ok(vec![]),
-        }
+        CloudRun::handle_error(resp, &CloudRun::response_to_list_jobs)
+    }
+
+    fn response_to_execution(
+        resp: (Response<Body>, GoogleCloudRunV2Execution),
+    ) -> Result<execution::Execution> {
+        execution::Execution::from_execution(&resp.1)
+    }
+    pub async fn executions_get(
+        &self,
+        execution_name: &execution::RunExecutionName,
+    ) -> Result<execution::Execution> {
+        let resp = self
+            .api
+            .projects()
+            .locations_jobs_executions_get(&execution_name.name())
+            .doit()
+            .await;
+        println!("{:?}", resp);
+        CloudRun::handle_error(resp, &CloudRun::response_to_execution)
+    }
+    pub async fn executions_delete(
+        &self,
+        execution_name: &execution::RunExecutionName,
+    ) -> Result<()> {
+        let resp = self
+            .api
+            .projects()
+            .locations_jobs_executions_delete(&execution_name.name())
+            .doit()
+            .await;
+        println!("{:?}", resp);
+        CloudRun::handle_error(resp, &CloudRun::response_to_operation)
+    }
+
+    fn response_to_list_executions(
+        resp: (Response<Body>, GoogleCloudRunV2ListExecutionsResponse),
+    ) -> Result<Vec<execution::Execution>> {
+        Ok(resp
+            .1
+            .executions
+            .as_ref()
+            .map(|executions| {
+                executions
+                    .iter()
+                    .map(|exe| execution::Execution::from_execution(exe).unwrap())
+                    .collect()
+            })
+            .unwrap_or_else(|| vec![]))
+    }
+
+    pub async fn executions_list(
+        &self,
+        execution_name: &execution::RunExecutionName,
+    ) -> Result<Vec<execution::Execution>> {
+        let resp = self
+            .api
+            .projects()
+            .locations_jobs_executions_list(&execution_name.parent())
+            .doit()
+            .await;
+        println!("{:?}", resp);
+        CloudRun::handle_error(resp, &CloudRun::response_to_list_executions)
     }
 }
--- src/run/job.rs
+++ src/run/job.rs
@@ -1,9 +1,7 @@
+use chrono::Duration;
+use chrono::{DateTime, Utc};
 use cloud_run::{
-    api::{
-        GoogleCloudRunV2CloudSqlInstance, GoogleCloudRunV2Container, GoogleCloudRunV2EnvVar,
-        GoogleCloudRunV2Job, GoogleCloudRunV2RevisionScaling, GoogleCloudRunV2RevisionTemplate,
-        GoogleCloudRunV2Volume, GoogleCloudRunV2VolumeMount,
-    },
+    api::{GoogleCloudRunV2ExecutionTemplate, GoogleCloudRunV2Job, GoogleCloudRunV2TaskTemplate},
     Error, Result as GcpResult,
 };
 use google_run2 as cloud_run;
@@ -27,6 +25,18 @@ impl RunJobName {
             project: project.to_string(),
             location: location.to_string(),
             job_name,
+        }
+    }
+
+    pub fn job_name(&self) -> Option<&String> {
+        self.job_name.as_ref()
+    }
+
+    pub fn replace_name(&self, job_name: String) -> Self {
+        RunJobName {
+            project: self.project.clone(),
+            location: self.location.clone(),
+            job_name: Some(job_name),
         }
     }
 
@@ -77,17 +87,90 @@ impl RunJobName {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Job {
-    name: RunJobName,
+    pub name: RunJobName,
     labels: HashMap<String, String>,
     parallelism: Option<i32>,
     task_count: Option<i32>,
+    max_retries: i32,
+    timeout: Option<i64>,
+    service_account: String,
     containers: Vec<super::Container>,
     volumes: Vec<super::Volume>,
-    timeout: i64,
-    service_account: String,
-    max_retries: i32,
+    update_time: Option<DateTime<Utc>>,
+    create_time: Option<DateTime<Utc>>,
 }
 
 impl Job {
-    pub fn from_job(job: &GoogleCloudRunV2Job) -> Result<Self> {}
+    pub fn to_job(&self) -> GoogleCloudRunV2Job {
+        let mut job = GoogleCloudRunV2Job::default();
+        job.labels = Some(self.labels.clone());
+        let mut template = GoogleCloudRunV2ExecutionTemplate::default();
+        let mut task_template = GoogleCloudRunV2TaskTemplate::default();
+        task_template.service_account = Some(self.service_account.clone());
+        task_template.timeout = self.timeout.map(|t| Duration::seconds(t));
+        task_template.max_retries = Some(self.max_retries);
+        task_template.containers = Some(self.containers.iter().map(|c| c.to_container()).collect());
+        task_template.volumes = Some(self.volumes.iter().map(|v| v.to_volume()).collect());
+        template.template = Some(task_template);
+        template.parallelism = self.parallelism.clone();
+        template.task_count = self.task_count.clone();
+        job.template = Some(template);
+        job
+    }
+    pub fn from_job(job: &GoogleCloudRunV2Job) -> Result<Self> {
+        if let Some(template) = job.template.as_ref() {
+            let name = job
+                .name
+                .as_ref()
+                .map(|name| RunJobName::from_name(&name).unwrap())
+                .unwrap();
+            let labels = job
+                .labels
+                .as_ref()
+                .map(|l| l.clone())
+                .unwrap_or_else(|| HashMap::new());
+            let parallelism = template.parallelism.clone();
+            let task_count = template.task_count.clone();
+            let create_time = job.create_time.clone();
+            let update_time = job.update_time.clone();
+            if let Some(task_template) = template.template.as_ref() {
+                let timeout = task_template.timeout.map(|t| t.num_seconds());
+                let max_retries = task_template.max_retries.unwrap();
+                let service_account = task_template.service_account.as_ref().unwrap().to_string();
+                let containers = if let Some(containers) = task_template.containers.as_ref() {
+                    containers
+                        .iter()
+                        .map(|container| super::Container::from_container(container))
+                        .collect()
+                } else {
+                    vec![]
+                };
+                let volumes = task_template
+                    .volumes
+                    .as_ref()
+                    .map(|vec| vec.clone())
+                    .unwrap_or(vec![])
+                    .iter()
+                    .map(|v| super::Volume::from_volume(&v))
+                    .collect();
+                Ok(Job {
+                    name,
+                    labels,
+                    parallelism,
+                    task_count,
+                    max_retries,
+                    timeout,
+                    service_account,
+                    containers,
+                    volumes,
+                    update_time,
+                    create_time,
+                })
+            } else {
+                Err(anyhow::anyhow!("task template does not exist"))
+            }
+        } else {
+            Err(anyhow::anyhow!("template does not exist"))
+        }
+    }
 }
